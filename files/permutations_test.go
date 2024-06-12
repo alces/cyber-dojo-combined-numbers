cd6d3b99e01c5ea15cb6fc50812af7f6e79a757a
xJAvuh <xJAvuh@cyber-dojo.org>
2024-06-12 11:50:13 +0000

    34 ran tests, predicted green, got green

--- files/permutations_test.go
+++ files/permutations_test.go
@@ -15,6 +15,9 @@ var permutationsResults = []struct {
     {
         []int{1,2}, [][]int{{1,2}, {2,1}},
     },
+    {
+        []int{1,2,3}, [][]int{{1,2,3}, {1,3,2}, {2,1,3}, {2,3,1}, {3,1,2}, {3,2,1}},
+    },
 }
 
 func TestPermutations(t *testing.T) {
