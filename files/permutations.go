7114a5302bd0bc8c86ae4c0ffc8ac59d5970c46f
xJAvuh <xJAvuh@cyber-dojo.org>
2024-06-12 10:00:29 +0000

    11 ran tests, predicted green, got green

--- files/permutations.go
+++ files/permutations.go
@@ -1,9 +1,5 @@
 package combine_numbers
 
 func withoutItem(slice []int, index int) []int {
-    if index == len(slice) - 1 {
-        return slice[:index]
-    }
-    
     return append(slice[:index], slice[index + 1:]...)
 }
