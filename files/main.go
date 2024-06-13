0b399492d6b0719b7c9ecb53137c38f4acafc03c
xJAvuh <xJAvuh@cyber-dojo.org>
2024-06-13 07:39:43 +0000

    40 ran tests, predicted amber, got green

--- files/main.go
+++ files/main.go
@@ -1,5 +1,17 @@
 package combine_numbers
 
+import (
+    "strconv"
+    "strings"
+)
+
 func joinNumbers(slice []int) string {
-    return ""
+    size := len(slice)
+    buffer := make([]string, size)
+    
+    for i := 0; i < size; i++ {
+        buffer[i] = strconv.Itoa(slice[i])
+    }
+    
+    return strings.Join(buffer, "")
 }
