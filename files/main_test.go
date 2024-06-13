cb473b9ee5d0855c08555a9feea6929fb8972455
xJAvuh <xJAvuh@cyber-dojo.org>
2024-06-13 07:50:13 +0000

    45 ran tests, predicted green, got green

--- files/main_test.go
+++ files/main_test.go
@@ -10,6 +10,16 @@ func TestJoinNumbers(t *testing.T) {
     assert.Equal(t, "12345", joinNumbers([]int{1, 23, 45}))
 }
 
+var largestTestCases = []struct {
+    slice    []int
+    expected string
+    message  string
+} {
+    {[]int{50, 2, 1, 9}, "95021", "[50, 2, 1, 9]"},
+}
+
 func TestLargest(t *testing.T) {
-    assert.Equal(t, "95021", Largest([]int{50, 2, 1, 9}))
+    for _, r := range largestTestCases {
+        assert.Equal(t, r.expected, Largest(r.slice), r.message)
+    }
 }
